961d7344ed80360f943f18285c862c33018e9407
meizolf <zsmeisam@gmail.com>
2021-07-29 13:01:28 +0430

    Initial commit

---
+++ src/main/java/com/example/shopdemo/ShopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shopdemo/ShopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
